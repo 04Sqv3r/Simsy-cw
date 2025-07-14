747c4cac93d1f563929ca24fc30b879e808f3927
KSH <kyrylo.shchedrin@studenci.collegiumwitelona.pl>
2025-07-14 03:44:36 -0700

    Update IShop.cs

--- Simsy/Interface/IShop.cs
+++ Simsy/Interface/IShop.cs
@@ -1,10 +1,10 @@
-﻿using Simsy.Models;
+using Simsy.Models;
 
 namespace Simsy.Interfaces
 {
     public interface IShop
     {
         List<Product> Products { get; set; }
-        bool Buy(Person character, Product product);
+        bool Buy(Character character, Product product);
     }
 }
